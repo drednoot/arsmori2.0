ba087dfe0d93fc488b3019a6982c4fa7c499a3c3
drednoot <matveyerohin228@gmail.com>
2022-01-18 15:36:04 +0400

    running a database fetch command
    okay that worked for some reason
    well whatever

--- hTelegram/__init__.py
+++ hTelegram/__init__.py
@@ -1,12 +1,25 @@
 import telebot
 from Constants.secret import SECRET
+from hTable import Animes
 
-bot = telebot.TeleBot(SECRET)
+class hTelegram:
+	def __init__(self, qh):
+		self.bot = telebot.TeleBot(SECRET)
+		self.qh = qh
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "test")
+		@self.bot.message_handler(commands=['start'])
+		def start(message):
+			self.bot.send_message(message.chat.id, "test")
 
-def run():
-	print("bot is running!")
-	bot.polling()
+
+		@self.bot.message_handler(commands=['anime'])
+		def anime(message):
+			haruhi = Animes(qh, 6)
+			names = haruhi.get_names().names
+			output = '\n'.join(names)
+			self.bot.send_message(message.chat.id, output)
+
+
+	def run(self):
+		print("bot is running!")
+		self.bot.polling()
