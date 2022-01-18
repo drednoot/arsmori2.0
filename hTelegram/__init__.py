254ededd649348737c30522b4c9a7c2dfcff7788
drednoot <matveyerohin228@gmail.com>
2022-01-18 15:56:47 +0400

    /anime function
    bruh

--- hTelegram/__init__.py
+++ hTelegram/__init__.py
@@ -1,11 +1,12 @@
 import telebot
 from Constants.secret import SECRET
-from hTable import Animes
+from hTelegram import telegram_functions as tfunc
 
 class hTelegram:
 	def __init__(self, qh):
 		self.bot = telebot.TeleBot(SECRET)
 		self.qh = qh
+
 
 		@self.bot.message_handler(commands=['start'])
 		def start(message):
@@ -14,12 +15,28 @@ class hTelegram:
 
 		@self.bot.message_handler(commands=['anime'])
 		def anime(message):
-			haruhi = Animes(qh, 6)
-			names = haruhi.get_names().names
-			output = '\n'.join(names)
-			self.bot.send_message(message.chat.id, output)
+			output = self.get_args(message.text)
+			if output != []:
+				id_ = output[0]
+				if id_.isnumeric():
+					output = tfunc.get_anime_names_from_id(self.qh, id_)
+					if output != "":
+						self.bot.send_message(message.chat.id, output)
+					else:
+						self.bot.send_message(message.chat.id, "bruh 404")
+				else:
+					self.bot.send_message(message.chat.id, "bruh send id and not some stupid bullshit you asshole")
+			else:
+				self.bot.send_message(message.chat.id, "bruh send smthng")
+
 
 
 	def run(self):
 		print("bot is running!")
 		self.bot.polling()
+
+
+	def get_args(self, string):
+		args = string.split()[1:]
+
+		return args
